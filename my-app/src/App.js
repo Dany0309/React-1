428f0a2ff916d572cb0182ab1f3f15bce9276b00
Dany0309 <142040968+Dany0309@users.noreply.github.com>
2024-09-10 21:23:58 +0500

    Урок #4
    Создали компонент пицц и категории, сделали рендер списка, сделали спред оператор

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,26 @@
 import React from "react";
 import "./scss/app.scss";
 
-import Header from './components/Header';
-import Sort from './components/Sort';
+import Header from "./components/Header";
+import Sort from "./components/Sort";
 import Categories from "./components/Categories";
 import PizzaBlock from "./components/PizzaBlock";
-
+import pizzas from "./pizzas.json";
 function App() {
   return (
     <div className="wrapper">
-      <Header/>
+      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories/>
-           <Sort/>
+            <Categories />
+            <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-           <PizzaBlock title="Мексиканская" price={500}/>
-           <PizzaBlock title="Мексиканская" price="500"/>
-           <PizzaBlock title="Мексиканская" price="500"/>
-           <PizzaBlock title="Мексиканская" price="500"/>
-           <PizzaBlock title="Мексиканская" price="500"/>
-           <PizzaBlock title="Мексиканская" price="500"/>
-           <PizzaBlock title="Мексиканская" price="500"/>
-           <PizzaBlock title="Мексиканская" price="500"/>
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
